545901289e774d06e1bbd0df8cdd9f43f459d2ab
George Henrique Wurthmann <gemakai@gmail.com>
2021-06-20 19:51:43 -0300

    Padronizando o retorno das requisições da API.

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,34 +1,32 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Customer } from '../models/customer.model';
+import { Result } from '../models/result.models';
 
 // localhost:3000/v1/customers
 @Controller('v1/customers')
 export class CustomerController {
   @Get()
   get() {
-    return 'Obter os cliente';
+    return new Result(null, true, [], null);
   }
 
   @Get(':document')
   getById(@Param('document') document) {
-    return 'Obter os cliente ' + document;
+    return new Result(null, true, {}, null);
   }
 
   @Post()
   post(@Body() body: Customer) {
-    return body;
+    return new Result('Cliente criado com sucesso', true, body, null);
   }
 
   @Put(':document')
   put(@Param('document') document, @Body() body: Customer) {
-    return {
-      customer: document,
-      data: body
-    };
+    return new Result('Cliente alterado com sucesso', true, body, null);
   }
 
   @Delete()
   delete(@Body() body: Customer) {
-    return body;
+    return new Result('Cliente removido com sucesso', true, body, null);
   }
 }
