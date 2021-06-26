d4263196f73d077dc42bde1444dd3a5353931c92
George Henrique Wurthmann <gemakai@gmail.com>
2021-06-25 21:59:55 -0300

    MongoDB user schema

--- src/backoffice/schemas/user.schema.ts
+++ src/backoffice/schemas/user.schema.ts
@@ -1,1 +1,22 @@
-// coming soon...
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        index: {
+            unique: true,
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+});
