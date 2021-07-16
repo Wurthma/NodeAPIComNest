a8fcad7e9b984e5959730d6523c2423f36b97f8a
George Henrique Wurthmann <gemakai@gmail.com>
2021-07-16 00:10:24 -0300

    Add Order and OrderItem entities

--- src/modules/store/store.module.ts
+++ src/modules/store/store.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './controllers/product.controller';
+import { OrderItem } from './entities/order-item.entity';
+import { Order } from './entities/order.entity';
 import { Product } from './entities/product.entity';
 import { ProductService } from './services/product.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([
         Product,
+        Order,
+        OrderItem,
     ])],
     providers: [
         ProductService,
