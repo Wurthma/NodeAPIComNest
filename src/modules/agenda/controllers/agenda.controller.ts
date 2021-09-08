1ddfe0531f7070d24d2c56ed1b786f0c4495b057
George Henrique Wurthmann <gemakai@gmail.com>
2021-09-07 22:00:55 -0300

    Agenda controller with CQRS

---
+++ src/modules/agenda/controllers/agenda.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { RoomBookService } from "../services/room-book.service";
+
+@Controller('backoffice/v1/rooms')
+export class AgendaController {
+    constructor(
+        private readonly service: RoomBookService
+    ) {}
+
+    @Post()
+    async Book(@Body() body: any) {
+        console.log('AppController:Book - Starting request');
+        await this.service.Book(body.customer, body.room);
+    }
+}
