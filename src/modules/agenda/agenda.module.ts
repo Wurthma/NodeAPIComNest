4093743b758b21172a621f8c874499d7600bc4df
George Henrique Wurthmann <gemakai@gmail.com>
2021-09-09 23:40:04 -0300

    Configure agenda.module

--- src/modules/agenda/agenda.module.ts
+++ src/modules/agenda/agenda.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { CommandHandlers } from './commands';
+import { AgendaController } from './controllers/agenda.controller';
+import { RoomRepository } from './repositories/room.repository';
+import { RoomBookService } from './services/room-book.service';
 
-@Module({})
+@Module({
+    imports: [CqrsModule],
+    controllers: [AgendaController],
+    providers: [
+        RoomBookService,
+        RoomRepository,
+        ...CommandHandlers
+    ]
+})
 export class AgendaModule {}
---
+++ src/modules/agenda/commands/index.ts
@@ -0,0 +1,3 @@
+import { BookRoomHandler } from "./handlers/book-room.handler";
+
+export const CommandHandlers = [BookRoomHandler];
